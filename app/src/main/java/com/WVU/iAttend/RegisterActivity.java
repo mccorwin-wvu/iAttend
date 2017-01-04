d76c4e6be6ccbb52c0197cd6bd91a316d8c2a2a3
mccorwin <mccorwin@mix.wvu.edu>
2017-01-04 17:18:05 -0500

    Made some new Classes and starting the log in system

--- app/src/main/java/com/WVU/iAttend/RegisterActivity.java
+++ app/src/main/java/com/WVU/iAttend/RegisterActivity.java
@@ -1,11 +1,21 @@
 package com.WVU.iAttend;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -45,6 +55,69 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText passConText = (EditText) findViewById(R.id.regPasswordCon);
 
         final Button buttonRegister = (Button) findViewById(R.id.regButton);
+
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String first_name = firstNameText.getText().toString();
+                final String last_name = lastNameText.getText().toString();
+                final String email = emailText.getText().toString();
+                final String password = passText.getText().toString();
+                final String password_con = passConText.getText().toString();
+
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success && password == password_con){
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("FAIL").setNegativeButton("Retry",null).create().show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                };
+
+
+
+
+
+                RegisterRequest registerRequest = new RegisterRequest(first_name,last_name,email,password,responseListener);
+
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
