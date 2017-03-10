bd89078256a4e9e76e4a2b8d5e9270e9dcd16da7
mccorwin <mccorwin@mix.wvu.edu>
2017-03-10 11:45:46 -0500

    create-a-class

---
+++ app/src/main/java/com/WVU/iAttend/CreateAClassActivity.java
@@ -0,0 +1,350 @@
+package com.WVU.iAttend;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.location.LocationListener;
+
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+
+
+public class CreateAClassActivity extends AppCompatActivity {
+
+    private double log = 0;
+    private double lat = 0;
+    private String joinCodeCode = "";
+
+    private double updatedLog = 0;
+    private double updatedLat = 0;
+
+
+    private boolean permissionGranted = false;
+
+    private Criteria criteria;
+    private String bestProvider;
+
+    private Button setLocationButton;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_aclass);
+
+
+        Typeface mytypeface = Typeface.createFromAsset(getAssets(), "Minecraftia-Regular.ttf");
+
+        TextView createAClassTextv = (TextView) findViewById(R.id.createAClassText);
+        createAClassTextv.setTypeface(mytypeface);
+
+        TextView ClassNamev = (TextView) findViewById(R.id.ClassName);
+        ClassNamev.setTypeface(mytypeface);
+
+        TextView StartTimeTextv = (TextView) findViewById(R.id.StartTimeText);
+        StartTimeTextv.setTypeface(mytypeface);
+
+        TextView endTImeTextv = (TextView) findViewById(R.id.endTImeText);
+        endTImeTextv.setTypeface(mytypeface);
+
+        TextView DaysOfTheWeekTextv = (TextView) findViewById(R.id.DaysOfTheWeekText);
+        DaysOfTheWeekTextv.setTypeface(mytypeface);
+
+        TextView radioButtonMonv = (TextView) findViewById(R.id.radioButtonMon);
+        TextView radioButtonTuesv = (TextView) findViewById(R.id.radioButtonTues);
+        TextView radioButtonWedv = (TextView) findViewById(R.id.radioButtonWed);
+        TextView radioButtonThursv = (TextView) findViewById(R.id.radioButtonThurs);
+        TextView radioButtonFriv = (TextView) findViewById(R.id.radioButtonFri);
+        TextView radioButtonSatv = (TextView) findViewById(R.id.radioButtonSat);
+        TextView radioButtonSunv = (TextView) findViewById(R.id.radioButtonSun);
+        radioButtonMonv.setTypeface(mytypeface);
+        radioButtonTuesv.setTypeface(mytypeface);
+        radioButtonWedv.setTypeface(mytypeface);
+        radioButtonThursv.setTypeface(mytypeface);
+        radioButtonFriv.setTypeface(mytypeface);
+        radioButtonSatv.setTypeface(mytypeface);
+        radioButtonSunv.setTypeface(mytypeface);
+
+
+        TextView StartDayTextv = (TextView) findViewById(R.id.StartDayText);
+        StartDayTextv.setTypeface(mytypeface);
+
+        TextView EndDayTextv = (TextView) findViewById(R.id.EndDayText);
+        EndDayTextv.setTypeface(mytypeface);
+
+        TextView JoinCodeTextv = (TextView) findViewById(R.id.JoinCodeText);
+        JoinCodeTextv.setTypeface(mytypeface);
+
+        TextView JoinCodev = (TextView) findViewById(R.id.JoinCode);
+        JoinCodev.setTypeface(mytypeface);
+
+        TextView LocationButtonv = (TextView) findViewById(R.id.LocationButton);
+        LocationButtonv.setTypeface(mytypeface);
+
+        TextView LocationSwitchv = (TextView) findViewById(R.id.LocationSwitch);
+        LocationSwitchv.setTypeface(mytypeface);
+
+        TextView CodeSwitchv = (TextView) findViewById(R.id.CodeSwitch);
+        CodeSwitchv.setTypeface(mytypeface);
+
+        TextView createButtonv = (TextView) findViewById(R.id.createButton);
+        createButtonv.setTypeface(mytypeface);
+
+
+        final EditText className = (EditText) findViewById(R.id.ClassName);
+        final TimePicker startTimePicker = (TimePicker) findViewById(R.id.StarttimePicker);
+        final TimePicker endTimePicker = (TimePicker) findViewById(R.id.endtimePicker);
+        final RadioButton mon = (RadioButton) findViewById(R.id.radioButtonMon);
+        final RadioButton tues = (RadioButton) findViewById(R.id.radioButtonTues);
+        final RadioButton wed = (RadioButton) findViewById(R.id.radioButtonWed);
+        final RadioButton thurs = (RadioButton) findViewById(R.id.radioButtonThurs);
+        final RadioButton fri = (RadioButton) findViewById(R.id.radioButtonFri);
+        final RadioButton sat = (RadioButton) findViewById(R.id.radioButtonSat);
+        final RadioButton sun = (RadioButton) findViewById(R.id.radioButtonSun);
+        final DatePicker startDate = (DatePicker) findViewById(R.id.StartDayPicker);
+        final DatePicker endDate = (DatePicker) findViewById(R.id.EndDatePicker);
+        final TextView joinCode = (TextView) findViewById(R.id.JoinCode);
+        setLocationButton = (Button) findViewById(R.id.LocationButton);
+        final Switch locationSwitch = (Switch) findViewById(R.id.LocationSwitch);
+        final Switch codeSwitch = (Switch) findViewById(R.id.CodeSwitch);
+        final Button createButton = (Button) findViewById(R.id.createButton);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        startDate.setMinDate(System.currentTimeMillis() - 1000);
+
+        joinCode.setText(joinCodeCode);
+
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                updatedLat = location.getLatitude();
+                updatedLog = location.getLongitude();
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 10);
+
+
+                return;
+            } else {
+
+                configureGps();
+
+            }
+        }
+
+        configureGps();
+        setLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                log = updatedLog;
+                lat = updatedLat;
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Longitude = " + log + "\n Latitude = " + lat,
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+
+
+            }
+
+
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                final String classNameFinal = className.getText().toString();
+                final String startTimeFinal = Integer.toString(startTimePicker.getCurrentHour()) +"$"+ Integer.toString(startTimePicker.getCurrentMinute());
+                final String endTimeFinal = Integer.toString(endTimePicker.getCurrentHour()) +"$"+ Integer.toString(endTimePicker.getCurrentMinute());
+                final String daysFinal ="";
+                final String startDayFinal=Integer.toString(startDate.getDayOfMonth())+"$"+Integer.toString(startDate.getMonth())+"$"+Integer.toString(startDate.getYear());
+                final String endDayFinal=Integer.toString(endDate.getDayOfMonth())+"$"+Integer.toString(endDate.getMonth())+"$"+Integer.toString(endDate.getYear());
+                final String joinCodeFinal = joinCodeCode;
+                final String location = log+"$"+lat;
+                final String locationEnabled;
+                final String codeEnabled;
+                boolean noError = true;
+                if(mon.isChecked()){daysFinal.concat("mon$");}if(tues.isChecked()){daysFinal.concat("tues$");}if(wed.isChecked()){daysFinal.concat("wed$");}if(thurs.isChecked()){daysFinal.concat("thurs$");}if(fri.isChecked()){daysFinal.concat("fri$");}if(sat.isChecked()){daysFinal.concat("sat$");}if(sun.isChecked()){daysFinal.concat("sun$");}
+                Calendar startDateCal = Calendar.getInstance();
+                Calendar endDateCal = Calendar.getInstance();
+                startDateCal.set(startDate.getYear(), startDate.getMonth(), startDate.getDayOfMonth());
+                endDateCal.set(endDate.getYear(), endDate.getMonth(), endDate.getDayOfMonth());
+
+                if(endDateCal.getTimeInMillis() - startDateCal.getTimeInMillis() < 604800000){
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "Start Date and End Date must be at least 1 week apart.",
+                            Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+
+                    noError = true;
+                    return;
+
+                }
+
+
+                if(locationSwitch.isChecked()){
+                    locationEnabled = "1";
+                }
+                else{
+                    locationEnabled = "0";
+                }
+
+                if(codeSwitch.isChecked()){
+                    codeEnabled = "1";
+                }
+                else{
+                    codeEnabled = "0";
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success){
+                                Intent intent = new Intent(CreateAClassActivity.this, UserHomePageActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+
+
+
+                                Toast toast = Toast.makeText(getApplicationContext(), "Class created.",
+                                        Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                toast.show();
+
+
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(CreateAClassActivity.this);
+                                builder.setMessage("Error, Class not Created").setNegativeButton("Retry",null).create().show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                };
+
+
+
+
+                if(noError == true){
+                    RegisterRequest registerRequest = new RegisterRequest(first_name,last_name,email,password, register_code, responseListener);
+
+                    RequestQueue queue = Volley.newRequestQueue(CreateAClassActivity.this);
+                    queue.add(registerRequest);}
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        });
+
+
+    }
+
+    private void configureGps() {
+
+        locationManager.requestLocationUpdates("gps", 1000, 0, locationListener);
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    configureGps();
+
+                    return;
+                }
+        }
+    }
+
+
+}
+
+
+
---
+++ app/src/main/java/com/WVU/iAttend/CreateAClassRequest.java
@@ -0,0 +1,42 @@
+package com.WVU.iAttend;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Matt on 1/4/2017.
+ */
+
+public class CreateAClassRequest extends StringRequest{
+
+    private static final String REGISTER_REQUEST_URL = "http://98.239.148.75/Register.php";
+    private Map<String,String> params;
+    public CreateAClassRequest(String class_name, String start_time, String end_time, String days_of_week, String start_date, String end_date,
+                               String join_code, String log, String lat, String loc_enabled, String code_enabled, String admin_id, String class_roster, Response.Listener<String> listener){
+
+        super(Method.POST, REGISTER_REQUEST_URL,listener,null);
+        params = new HashMap<>();
+        params.put("class_name",class_name);
+        params.put("start_time",start_time);
+        params.put("end_time",end_time);
+        params.put("days_of_week",days_of_week);
+        params.put("start_date",start_date);
+        params.put("end_date",end_date);
+        params.put("join_code",join_code);
+        params.put("log",log);
+        params.put("lat",lat);
+        params.put("loc_enabled",loc_enabled);
+        params.put("code_enabled",code_enabled);
+        params.put("admin_id",admin_id);
+        params.put("class_roster",class_roster);
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
