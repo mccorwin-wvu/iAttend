476e9e76d258965210aa145b9a53d0f44c558628
emarsh <edmarshall@mix.wvu.edu>
2017-03-10 11:50:39 -0500

    added scrolling layout for text fields

---
+++ app/src/main/java/com/WVU/iAttend/MyClassesActivity.java
@@ -0,0 +1,13 @@
+package com.WVU.iAttend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyClassesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_classes);
+    }
+}
