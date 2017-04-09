50fa521b69b50ce80495dba4b4da069c1a8e1e29
docam <docampbell@mix.wvu.edu>
2017-04-09 12:23:36 -0400

    i did stuff and things. Ethan says hello.

--- app/src/main/java/com/WVU/iAttend/ClickableList.java
+++ app/src/main/java/com/WVU/iAttend/ClickableList.java
@@ -11,29 +11,29 @@ import android.widget.ArrayAdapter;
  */
 
 class ClickableList extends ListActivity {
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState){
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_clickable_list);
-//
-//        String[] cars = {"Dodge", "Chevy", "Toyota"};
-//        ArrayAdapter<String> adapter= new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, cars);
-//        getListView().setAdapter(adapter);
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu){
-//        //Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item){
-//        int id = item.getItemId();
-//        if(id == R.id.action_settings){
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clickable_list);
+
+        String[] cars = {"Dodge", "Chevy", "Toyota"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, cars);
+        getListView().setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
