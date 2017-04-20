0fdd58c75a4672cc06a627caefe67cd90a945bd6
mccorwin <mccorwin@mix.wvu.edu>
2017-04-20 00:55:57 -0400

    commented code in LoginActivity

--- app/src/main/java/com/WVU/iAttend/LoginActivity.java
+++ app/src/main/java/com/WVU/iAttend/LoginActivity.java
@@ -24,13 +24,18 @@ import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int CONFIRMED = 1;
+    private static final int NOTCONFIRMED = 1;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
 
-
+        // Setting the fount for all the Views in the activity
 
         Typeface mytypeface = Typeface.createFromAsset(getAssets(),"Minecraftia-Regular.ttf");
 
@@ -52,14 +57,18 @@ public class LoginActivity extends AppCompatActivity {
         TextView forgotPasswordHomePagev = (TextView) findViewById(R.id.forgotPasswordHomePage);
         forgotPasswordHomePagev.setTypeface(mytypeface);
 
-        final EditText loginEmail = (EditText) findViewById(R.id.logingEmailBox);
-        final EditText loginPass = (EditText) findViewById(R.id.logingPassBox);
-        final Button loginButton = (Button) findViewById(R.id.logingButton);
-        final TextView registerText = (TextView) findViewById(R.id.logingReg);
-        final TextView forgotPasswordText = (TextView) findViewById(R.id.forgotPasswordHomePage);
+
+        // making a separate variable of each view to use in the rest of the method
+
+        final EditText loginEmail = (EditText) logingEmailBoxv;
+        final EditText loginPass = (EditText) logingPassBoxv;
+        final Button loginButton = (Button) logingButtonv;
+        final TextView registerText = logingRegv;
+        final TextView forgotPasswordText = forgotPasswordHomePagev;
 
 
 
+        // sends the user to the forgot password activity
 
         forgotPasswordText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +78,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // sends the user to the register activity
 
         registerText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,29 +86,46 @@ public class LoginActivity extends AppCompatActivity {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
 
-
-
             }
         });
 
+
+        // when the login button is pressed the app gets the email and password entered and checks if they match with the email and password in the database
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // contains the the current contents of the email and password box at the time the login button is pressed
 
                 final String email = loginEmail.getText().toString();
                 final String password = loginPass.getText().toString();
+
+
+                // the response from the server that returns a string of JSON responses
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
 
+
+                    // try block to catch any exceptions with the JSON string
+
                     try {
+
+                        // a JSON response object that contains all the data
+
                         JSONObject jsonResponse = new JSONObject(response);
+
+                        // a boolean from the server to make sure the email entered is in the database
+
                         boolean success = jsonResponse.getBoolean("success");
 
+                        // if the email is in the database
+
                         if(success){
+
+                            // get data from the JSON string and store it in its own variable
 
                             int user_id = jsonResponse.getInt("user_id");
                             String first_name = jsonResponse.getString("first_name");
@@ -112,7 +139,12 @@ public class LoginActivity extends AppCompatActivity {
                             String user_class_list = jsonResponse.getString("user_class_list");
 
 
-                            if(confirmed == 0){
+                            // checks to see if the user is confirmed
+
+                            if(confirmed == NOTCONFIRMED){
+
+
+                                // if the user entered is not confirmed then it prints a Toast and sends the user to the Confirm user activity
 
                                 Intent intent = new Intent(LoginActivity.this,ConfirmNewUser.class);
 
@@ -125,7 +157,13 @@ public class LoginActivity extends AppCompatActivity {
 
 
                             }
+
+                            // else if the user is confirmed
+
                             else {
+
+                                // puts all the needed data into an intent object and sends that object along with the user to the User Home Page activity
+
                                 Intent intent = new Intent(LoginActivity.this, UserHomePageActivity.class);
                                 intent.putExtra("user_id", user_id);
                                 intent.putExtra("first_name", first_name);
@@ -144,12 +182,21 @@ public class LoginActivity extends AppCompatActivity {
 
                         }
 
+
+                        // if the email or password does not match anything in the database
+
                         else{
+
+                            // creates an alert dialog telling the user that they entered an invalid email or password
+
                             AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                             builder.setMessage("Invalid Email or Password").setNegativeButton("Retry",null).create().show();
                         }
 
                     }
+
+                    // catches JSON exceptions
+
                     catch (JSONException e){
                         e.printStackTrace();
 
@@ -159,6 +206,8 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 };
 
+
+                // loginRequest sends the email and password to the server and will trigger responseLister when the server responds
 
                 LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
